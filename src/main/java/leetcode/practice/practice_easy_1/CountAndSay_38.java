b4d66f2804f2075e3f327e708c7e93f86087db37
Viktar <mamai217vs@gmail.com>
2019-11-03 14:33:16 +0300

    Leetcode tasks 26, 38, 67

---
+++ src/main/java/leetcode/practice/practice_easy_1/CountAndSay_38.java
@@ -0,0 +1,30 @@
+package leetcode.practice.practice_easy_1;
+
+public class CountAndSay_38 {
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        String prevStr = "1";
+
+        StringBuilder builder;
+        for (int i = 2; i <= n; i++) {
+            int count = 1;
+            char prevChar = prevStr.charAt(0);
+            builder = new StringBuilder();
+            for (int k = 1; k < prevStr.length(); k++) {
+                char ch = prevStr.charAt(k);
+                if (ch == prevChar) {
+                    count++;
+                } else {
+                    builder.append(count).append(prevChar);
+                    prevChar = ch;
+                    count = 1;
+                }
+            }
+            builder.append(count).append(prevChar);
+            prevStr = builder.toString();
+        }
+        return prevStr;
+    }
+}
---
+++ src/main/java/leetcode/practice/practice_easy_1/RemoveDuplicates_26.java
@@ -0,0 +1,31 @@
+package leetcode.practice.practice_easy_1;
+
+public class RemoveDuplicates_26 {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if(n <= 1) return n;
+        int a1 = nums[0];
+        int count = 1;
+        int right = n-1;
+        int i;
+        for(i = 1; i<=right;) {
+            if(nums[i] == a1) {
+                bubble(nums, i, right);
+                right--;
+            } else {
+                a1 = nums[i];
+                i++;
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void bubble(int[] nums, int i, int right) {
+        int val = nums[i];
+        for(int k = i; k<right; k++) {
+            nums[k] = nums[k+1];
+        }
+        nums[right] = val;
+    }
+}
